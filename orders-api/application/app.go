f792c7313e72cfc2666c6b655078a8c885bea980
rodatboat <ronaldo3055@gmail.com>
2024-10-12 17:06:59 -0500

    finished redis setup

--- orders-api/application/app.go
+++ orders-api/application/app.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -33,11 +34,38 @@ func (a *App) Start(ctx context.Context) error {
 		return fmt.Errorf("failed to ping redis: %w", err)
 	}
 
+	defer func() {
+		if err := a.rdb.Close(); err != nil {
+			fmt.Println("failed to close redis", err)
+		}
+	}()
+
 	fmt.Println("Starting server...")
 
-	err = server.ListenAndServe()
+	ch := make(chan error, 1)
+
+	go func() {
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to start server: %w", err)
+		}
+		close(ch)
+	}()
+
+	ctx.Done()
+	err = <-ch
 	if err != nil {
 		return fmt.Errorf("failed to start server: %w", err)
+	}
+
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		return server.Shutdown(timeout)
 	}
 
 	return nil
