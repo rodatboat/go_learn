356fc684bdb7badf0561b6ded53412b312abbbce
Ronaldo Suarez <ronaldo3055@gmail.com>
2024-10-12 15:28:26 -0500

    #3 @ 10:30

--- orders-api/main.go
+++ orders-api/main.go
@@ -4,13 +4,17 @@ import (
 	"context"
 	"fmt"
 	"go_learn/orders-api/application"
+	"os"
+	"os/signal"
 )
 
 func main() {
 	app := application.New()
-	
-	err := app.Start(context.TODO())
 
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel() // defer means to call it at the end of the current function
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("failed to start app:", err)
 	}
